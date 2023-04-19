5acd994e02b7335c80d1f476f5d8be6896def3bc
Benjamín Vicente <benjavicente@uc.cl>
2023-04-19 11:55:49 -0400

    input más claro

--- app/main.js
+++ app/main.js
@@ -1,7 +1,42 @@
 import "./style.css";
 
 import emscripten from "../func/emscripten/scheduler.js";
-import * as javascriptNormal from "../func/schedulerNormal.js";
-import * as javascriptOptimus from "../func/schedulerOptimus.js";
+import { assignJobs as javascriptNormal } from "../func/schedulerNormal.js";
+import { assignJobs as javascriptOptimus } from "../func/schedulerOptimus.js";
 
-window.addEventListener("load", async () => await emscripten());
+async function runAndDisplay(name, fn, bins, durations) {
+  // console.log("Running", name, bins, durations);
+  console.time(name);
+  await fn(bins, durations);
+  console.timeEnd(name);
+}
+
+window.addEventListener("load", async () => {
+  const runForm = document.getElementById("run-form");
+
+  const functions = {
+    "js-slow": javascriptNormal,
+    "js-optimus": javascriptOptimus,
+    "cpp-emscripten": async (bins, durations) => {
+      await emscripten();
+    },
+  };
+
+  runForm.addEventListener("submit", (event) => {
+    event.preventDefault();
+    const { submitter, target } = event;
+
+    const name = submitter.dataset["fn"];
+    const fn = functions[name];
+    if (!fn) throw new Error(`Invalid: ${submitter.dataset["fn"]}`);
+
+    const data = new FormData(target);
+
+    const binsStr = data.get("bins");
+    const bins = parseInt(binsStr, 10);
+    const durationsStr = data.get("durations");
+    const durations = durationsStr.split(",").map((s) => parseInt(s, 10));
+
+    runAndDisplay(name, fn, bins, durations);
+  });
+});
--- func/schedulerNormal.js
+++ func/schedulerNormal.js
@@ -1,4 +1,4 @@
-function assignJobs(N, durations, M) {
+export function assignJobs(M, durations) {
   const totalTimes = new Array(M).fill(0);
   const clusters = new Array(M).fill(null).map(() => []);
   const jobs = durations.map((duration, index) => ({ duration, index })).sort((a, b) => b.duration - a.duration);
@@ -10,13 +10,3 @@ function assignJobs(N, durations, M) {
   const totalTime = Math.max(...totalTimes);
   return { clusters, totalTime };
 }
-
-console.log('JSNormal');
-
-const N = 5;
-const durations = [30, 50, 10, 20, 90];
-const M = 2;
-
-const result = assignJobs(N, durations, M);
-console.log(result.clusters);
-console.log(result.totalTime);
