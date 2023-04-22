d5f7f04daf5ea01b8e5e881422be4729a04c58de
Benjamín Vicente <benjavicente@uc.cl>
2023-04-22 15:39:27 -0400

    prime

--- func/schedulerOptimus.js
+++ func/schedulerOptimus.js
@@ -1,16 +1,36 @@
 export function assignJobs(M, durations) {
-  // console.log("[JSO] Log schedulerOptimus.js");
-  const totalTimes = new Array(M).fill(0);
+  // Los clusters se almacenan en un arreglo que siempre se mantendrá ordenado
+  const orderedClusters = new Array(M).fill(null);
+  for (let i = 0; i < M; i++) orderedClusters[i] = { index: i, totalTime: 0, jobs: [] };
 
-  const clusters = new Array(M).fill(null);
-  for (let i = 0; i < M; i++) clusters[i] = [];
-
-  const jobs = durations.map((duration, index) => ({ duration, index })).sort((a, b) => b.duration - a.duration);
-  for (const job of jobs) {
-    const minTotalTimeCluster = totalTimes.indexOf(Math.min(...totalTimes));
-    clusters[minTotalTimeCluster].push(job.index);
-    totalTimes[minTotalTimeCluster] += job.duration;
+  // Insertion sort de los jobs, mapeando a un objeto
+  const jobs = [{ duration: durations[0], index: 0 }];
+  for (let i = 1; i < durations.length; i++) {
+    const job = { duration: durations[i], index: i };
+    for (let j = 0; j < jobs.length; j++) {
+      if (jobs[j].duration < job.duration) {
+        jobs.splice(j, 0, job);
+        break;
+      }
+    }
+    if (jobs.length < i + 1) jobs.push(job);
   }
-  const totalTime = Math.max(...totalTimes);
-  return totalTime;
+
+  for (const job of jobs) {
+    // El primero siempre tendrá el menor tiempo total
+    orderedClusters[0].jobs.push(job.index);
+    orderedClusters[0].totalTime += job.duration;
+
+    // Movemos el elemento modificado al lugar correcto
+    for (let i = 0; i < orderedClusters.length - 1; i++) {
+      if (orderedClusters[i].totalTime > orderedClusters[i + 1].totalTime) {
+        const temp = orderedClusters[i];
+        orderedClusters[i] = orderedClusters[i + 1];
+        orderedClusters[i + 1] = temp;
+      } else break;
+    }
+  }
+
+  // El último elemento será el máximo
+  return orderedClusters[M - 1].totalTime;
 }
