0b9e5d6f67894ec84d1375b74c328babf8d14357
Jose Antonio Castro <jacastro18@uc.cl>
2023-04-22 21:28:59 -0400

    fix conflicts

--- app/main.ts
+++ app/main.ts
@@ -5,7 +5,7 @@ import { FunctionName } from "./worker";
 
 import "./style.css";
 
-const functionNames: FunctionName[] = ["js-normal", "js-optimus", "cpp-emscripten", "rust-wasm-pack", "assemblyscript", "go-wasm"];
+const functionNames: FunctionName[] = ["js-normal", "cpp-emscripten", "rust-wasm-pack", "assemblyscript", "js-optimus", "go-wasm"];
 
 window.addEventListener("load", async () => {
   const runForm = document.getElementById("run-form")!;
