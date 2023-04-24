d3affdcaa81b0fb8f815d1e554cf0133657c0275
Benjamín Vicente <benjavicente@uc.cl>
2023-04-24 15:02:00 -0400

    update vite headers

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,12 @@ import VitePluginWasm from "vite-plugin-wasm";
 export default defineConfig({
   base: process.env.REPO_NAME || "/",
   plugins: [VitePluginWasm()],
+  server: {
+    headers: {
+      "Cross-Origin-Opener-Policy": "same-origin",
+      "Cross-Origin-Embedder-Policy": "require-corp",
+    },
+  },
   build: {
     target: "esnext",
   },
